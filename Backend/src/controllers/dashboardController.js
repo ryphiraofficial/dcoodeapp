ee2b71f54c01280ed8473416cfaed60abcdf44d5
Rohith <rohith@dcoode.com>
2026-06-26 12:57:31 +0530

    chore: push backend files

--- Backend/src/controllers/dashboardController.js
+++
@@ -1,9 +0,0 @@
-import * as dashboardService from '../services/dashboardService.js';
-import { sendSuccess } from '../utils/response.js';
-
-export const getDashboard = async (req, res, next) => {
-  try {
-    const data = await dashboardService.getDashboard();
-    return sendSuccess(res, 200, 'Dashboard data fetched successfully', data);
-  } catch (err) { next(err); }
-};
--- Backend/src/utils/generatePassword.js
+++
@@ -1,9 +0,0 @@
-/** Generates a random secure alphanumeric password (8-10 characters) */
-export const generatePassword = (length = 10) => {
-  const chars = 'ABCDEFGHJKLMNPQRSTUVWXYZabcdefghjkmnpqrstuvwxyz23456789!@#$';
-  let password = '';
-  for (let i = 0; i < length; i++) {
-    password += chars.charAt(Math.floor(Math.random() * chars.length));
-  }
-  return password;
-};
